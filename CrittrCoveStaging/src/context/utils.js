0419416170e2f805372068d2afdf7bf8286e54dd
aiintegrations <zam.hh.llc@gmail.com>
2025-02-12 19:38:25 -0700

    Deploy web build

--- CrittrCoveStaging/src/context/utils.js
+++
@@ -1,8 +0,0 @@
-import { format, parse } from 'date-fns';
-
-export const convertTo12HourFormat = (time) => {
-  // Parse the time string to a Date object
-  const date = parse(time, 'HH:mm', new Date());
-  // Format the date to 12-hour format
-  return format(date, 'h:mm a');
-};
