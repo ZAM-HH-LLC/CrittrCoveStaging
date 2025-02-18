b335ed8348b28830788018a261634c8a7aa697c3
aiintegrations <zam.hh.llc@gmail.com>
2025-02-18 12:08:24 -0700

    adding unit of time to booking details admin.

--- backend/zenexotics_backend/booking_details/admin.py
+++ backend/zenexotics_backend/booking_details/admin.py
@@ -3,14 +3,18 @@ from .models import BookingDetails
 
 @admin.register(BookingDetails)
 class BookingDetailsAdmin(admin.ModelAdmin):
-    list_display = ('detail_id', 'booking_occurrence', 'num_pets', 'base_rate', 'additional_pet_rate', 'holiday_rate', 'calculated_rate')
+    list_display = ('detail_id', 'booking_occurrence', 'num_pets', 'base_rate', 'additional_pet_rate', 'holiday_rate', 'calculated_rate', 'get_unit_of_time')
     search_fields = ('booking_occurrence__booking__client__user__email', 'booking_occurrence__booking__professional__user__email')
-    readonly_fields = ('calculated_rate',)
+    readonly_fields = ('calculated_rate', 'get_unit_of_time')
     fieldsets = (
         ('Basic Information', {
             'fields': ('booking_occurrence', 'num_pets')
         }),
         ('Rates', {
-            'fields': ('base_rate', 'additional_pet_rate', 'applies_after', 'holiday_rate', 'calculated_rate')
+            'fields': ('base_rate', 'additional_pet_rate', 'applies_after', 'holiday_rate', 'calculated_rate', 'get_unit_of_time')
         }),
     )
+
+    def get_unit_of_time(self, obj):
+        return obj.booking_occurrence.booking.service_id.unit_of_time
+    get_unit_of_time.short_description = 'Unit of Time'
