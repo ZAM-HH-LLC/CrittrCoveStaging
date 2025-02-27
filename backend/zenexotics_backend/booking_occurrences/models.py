e714ed04bf85079f51f0a800cc080cd3605333a8
aiintegrations <zam.hh.llc@gmail.com>
2025-02-27 12:36:29 -0700

    fixing booking details unit_of_time

--- backend/zenexotics_backend/booking_occurrences/models.py
+++ backend/zenexotics_backend/booking_occurrences/models.py
@@ -214,5 +214,6 @@ def create_booking_details(sender, instance, created, **kwargs):
             additional_pet_rate=service.additional_animal_rate,
             holiday_rate=service.holiday_rate,
             applies_after=service.applies_after,
+            unit_of_time=service.unit_of_time,
             calculated_rate=Decimal('0.00')  # This will be updated by its own save method
         )
