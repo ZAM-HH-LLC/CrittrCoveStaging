9ad3e41da8a5bde0fe7bab6924f5895b818cef3a
aiintegrations <zam.hh.llc@gmail.com>
2025-03-03 22:50:42 -0700

    removing old draft update function

--- backend/zenexotics_backend/booking_drafts/v1/urls.py
+++ backend/zenexotics_backend/booking_drafts/v1/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:booking_id>/update/', views.BookingDraftUpdateView.as_view(), name='booking_draft_update'),
     path('<int:booking_id>/available_pets/', views.AvailablePetsView.as_view(), name='booking_draft_available_pets'),
     path('<int:booking_id>/update_occurrences/', views.UpdateBookingOccurrencesView.as_view(), name='booking_draft_update_occurrences'),
     path('<int:booking_id>/update_pets/', views.UpdateBookingPetsView.as_view(), name='booking_draft_update_pets'),
