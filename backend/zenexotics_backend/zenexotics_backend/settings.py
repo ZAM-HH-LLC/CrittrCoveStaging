9e55ff81482e28459a54e0bc09e5a70e2cf7bd70
aiintegrations <zam.hh.llc@gmail.com>
2025-04-22 12:43:33 -0600

    making progress on zoho email setup

--- backend/zenexotics_backend/zenexotics_backend/settings.py
+++ backend/zenexotics_backend/zenexotics_backend/settings.py
@@ -243,16 +243,16 @@ CORS_ALLOWED_ORIGINS = [
 
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'zam.hh.llc@gmail.com'
-EMAIL_HOST_PASSWORD = 'tnrz nsbl uhiu dmxb'  # Gmail app password
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.zoho.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = False
 
 # Contact and sender emails
-CONTACT_EMAIL = 'zam.hh.llc@gmail.com'
-DEFAULT_FROM_EMAIL = 'CrittrCove <zam.hh.llc@gmail.com>'  # Format with name for better deliverability
+CONTACT_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = f'CrittrCove <{EMAIL_HOST_USER}>'  # Format with name for better deliverability
 
 # Email deliverability settings
 EMAIL_TIMEOUT = 30  # Timeout in seconds
@@ -327,6 +327,9 @@ CORS_ALLOW_HEADERS = [
 
 # Django Channels
 ASGI_APPLICATION = "zenexotics_backend.asgi.application"
+
+# In settings.py
+SENDGRID_API_KEY = 'SG.1234567890'
 
 # Channel Layers
 CHANNEL_LAYERS = {
