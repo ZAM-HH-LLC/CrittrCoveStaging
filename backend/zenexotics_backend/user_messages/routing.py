3c95f2b45f32e83b2d47327676db7b57bf4b6b08
aiintegrations <zam.hh.llc@gmail.com>
2025-05-27 18:14:31 -0600

    committing routing file for websockets

--- backend/zenexotics_backend/user_messages/routing.py
+++ backend/zenexotics_backend/user_messages/routing.py
@@ -4,11 +4,7 @@ from .middleware import JWTAuthMiddleware
 from channels.routing import URLRouter
 from channels.auth import AuthMiddlewareStack
 
-# Define WebSocket URL patterns
+# Define WebSocket URL patterns with JWT authentication
 websocket_urlpatterns = [
-    re_path(r"ws/messages/$", JWTAuthMiddleware(AuthMiddlewareStack(
-        URLRouter([
-            re_path(r"^$", consumers.MessageConsumer.as_asgi()),
-        ])
-    ))),
+    re_path(r'ws/messages/$', JWTAuthMiddleware(consumers.MessageConsumer.as_asgi())),
 ] 
