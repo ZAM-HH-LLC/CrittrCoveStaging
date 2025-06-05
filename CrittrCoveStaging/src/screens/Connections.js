5addf50bc5d0f3b0ff9864795f77f47347b4adcb
Matt Aertker <zam.hh.llc@gmail.com>
2025-06-05 16:42:16 -0600

    fixing the invitation sent email confirmation message.

--- CrittrCoveStaging/src/screens/Connections.js
+++ CrittrCoveStaging/src/screens/Connections.js
@@ -301,7 +301,7 @@ const Connections = () => {
       debugLog('MBA4321 Invitation created successfully:', response);
       
       if (invitationType === 'email') {
-        setInviteSuccess(`Invitation sent to ${inviteEmail} - please have your client check spam if you don't see it in your inbox.`);
+        setInviteSuccess(`Invitation sent to ${inviteEmail} - please have your client check spam if they don't see it in their inbox.`);
         setInviteEmail('');
       } else {
         setGeneratedLink(response.invitation_link);
