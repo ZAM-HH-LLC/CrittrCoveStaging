be2b3fd0dc35d63236a9650c7f98e30632b2c020
Matt Aertker <zam.hh.llc@gmail.com>
2025-08-15 16:12:09 -0600

    restoring favicon, and editing package.json and index.html and static seo pages.

--- CrittrCoveStaging/scripts/generateSEOStatic.js
+++ CrittrCoveStaging/scripts/generateSEOStatic.js
@@ -385,10 +385,13 @@ const generateHTMLTemplate = (page, buildFiles) => {
 <head>
     <meta charset="utf-8" />
     <link rel="icon" href="/favicon.ico" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/android-chrome-192x192.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/android-chrome-192x192.png" />
     <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
     <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <link rel="manifest" href="/site.webmanifest" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
     <meta name="description" content="${page.description}" />
